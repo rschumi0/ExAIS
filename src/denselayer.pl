8d0445a3df57a92b9ef4cfcbf51ea192fab4e180
Ayesha Sadiq <Ayesha.Sadiq@monash.edu>
2020-09-02 20:05:37 +1000

    denselayers updated

--- src/denselayer.pl
+++ src/denselayer.pl
@@ -1,16 +1,13 @@
-%?- dense_layer2D([4,9],[[5,8],[7,6]],[4,3],X). 
+%?- dense_layer1D([4,9],[[5,8],[7,6]],[4,3],X). 
 % X = [87, 89] 
 
-%?- dense_layer_randIs(2,10,[[5,8],[7,6]],[4,3],X).
-%X = [60, 77] 
-
-%?- dense_layer3D([1,2,3],[[2,3,4],[5,4,6],[7,6,8]],[2,3,4],X).
+%?- dense_layer1D([1,2,3],[[2,3,4],[5,4,6],[7,6,8]],[2,3,4],X).
 % X = [35, 32, 44] 
 
-% ?-dense_layer3D([4,3,4,5],[[5,8,5],[7,6,6],[3,5,7],[3,5,4]],[4,3,5,6],X).
+% ?-dense_layer1D([4,3,4,5],[[5,8,5],[7,6,6],[3,5,7],[3,5,4]],[4,3,5,6],X).
 % X = [72, 98, 91] 
 
-% ?- dense_layer3D([4,3,4,5,6],[[5,8,5],[7,6,6],[3,5,7],[3,5,4],[3,4,5]],[4,3,5,6,7],X).
+% ?- dense_layer1D([4,3,4,5,6],[[5,8,5],[7,6,6],[3,5,7],[3,5,4],[3,4,5]],[4,3,5,6,7],X).
 % X = [90, 122, 121] 
 
 % output = activation(dot(input, weight_data) + bias)
@@ -18,7 +15,7 @@
 
 :- use_module(library(clpfd)).
 
-dense_layer3D(Is,IWs,OWs,Os) :- 
+dense_layer1D(Is,IWs,OWs,Os) :- 
    transpose(IWs,IWs1),  
    dense_layer(Is,IWs1,OWs,[],Os).  
 
@@ -36,5 +33,3 @@ cal_output(Is,IWs,OW,Res) :-
    output_comp([I|Is],[IW|IWs],OW,Res0,Res) :- 
    Res1 is Res0 + I * IW,
    output_comp(Is,IWs,OW,Res1,Res).
-
-   
