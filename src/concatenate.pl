6a07a3a73d48122284c54c63bd50f927718c6a26
Ayesha Sadiq <Ayesha.Sadiq@monash.edu>
2020-10-10 03:33:22 +1100

    concatenate layer added

--- src/concatenate.pl
+++ src/concatenate.pl
@@ -1,5 +1,5 @@
 
-% tf.keras.layers.Concatenate( axis=-1, **kwargs)
+% tf.keras.layers.Concatenate( axis = -1, **kwargs)
 % It takes as input a list of tensors, 
 % all of the same shape except for the concatenation axis, 
 % and returns a single tensor that is the concatenation of all inputs.
@@ -7,12 +7,19 @@
 :- use_module(library(lists)).
 :- [util].
 
+% concatenate_lists_1D([1,2],[3,4],X).X = [1, 2, 3, 4].
+% X = [1, 2, 3, 4].
 
 concatenate_lists_1D([],[],[]).
 concatenate_lists_1D(X,[],X).
 concatenate_lists_1D([],Z,Z).
 concatenate_lists_1D(X,Y,Z):-
  append(X,Y,Z).
+
+% ?- concatenate_lists_2D([[1,2],[3,4]],[[6,7],[9,8]],X).
+% X = [[1, 2, 6, 7], [3, 4, 9, 8]] ;
+% ?- concatenate_lists_2D([[1,2,4],[3,4,5]],[[6,7,6],[9,8,8]],X).
+% X = [[1, 2, 4, 6, 7, 6], [3, 4, 5, 9, 8, 8]] ;
 
 concatenate_lists_2D([],[],[]).
 concatenate_lists_2D(X,[],X).
@@ -21,6 +28,11 @@ concatenate_lists_2D([X|Xs],[Y|Ys],Z):-
  concatenate_lists_1D(X,Y,Zs),
  Z = [Zs|T],  
  concatenate_lists_2D(Xs, Ys, T).
+
+% concatenate_lists_3D([[[1,0],[2,0]],[[1,2],[2,3]]],[[[1,2],[3,4]],[[1,2],[3,4]]],[],X).
+% X = [[[1, 0], [2, 0], [1, 2], [3, 4]], [[1, 2], [2, 3], [1, 2], [3, 4]]] ;
+% concatenate_lists_3D([[[0,1,3],[5,6,3]],[[10,11,3],[15,16,3]]],[[[20,21,3],[25,26,3]],[[30,31,3],[35,36,3]]],[],X).
+% X = [[[0, 1, 3], [5, 6, 3], [20, 21, 3], [25, 26, 3]], [[10, 11, 3], [15, 16, 3], [30, 31, 3], [35, 36, 3]]] ;
 
 concatenate_lists_3D([], [], Z, Z).
 concatenate_lists_3D(X,  [], _, X).
