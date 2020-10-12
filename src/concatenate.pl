9625320bc5a696606aa43d6ebfea0a38498c11d2
Ayesha Sadiq <Ayesha.Sadiq@monash.edu>
2020-10-13 09:57:34 +1100

    concatenation finsihed

--- src/concatenate.pl
+++ src/concatenate.pl
@@ -10,27 +10,38 @@
 % concatenate_lists_1D([1,2],[3,4],X).X = [1, 2, 3, 4].
 % X = [1, 2, 3, 4].
 
-concatenate_lists_1D([],[],[]).
-concatenate_lists_1D(X,[],X).
-concatenate_lists_1D([],Z,Z).
-concatenate_lists_1D(X,Y,Z):-
+% ?- concatenate_lists_2_3_D_axis1([1,2,3,4],[6,7,9,8],X).% there is no axis = 1 
+% false.
+
+concatenate_lists_1D_axis0([],[],[]).
+concatenate_lists_1D_axis0(X,[],X).
+concatenate_lists_1D_axis0([],Z,Z).
+concatenate_lists_1D_axis0(X,Y,Z):-
  append(X,Y,Z).
 
-% ?- concatenate_lists_2D([[1,2],[3,4]],[[6,7],[9,8]],X).
-% X = [[1, 2, 6, 7], [3, 4, 9, 8]] ;
-% ?- concatenate_lists_2D([[1,2,4],[3,4,5]],[[6,7,6],[9,8,8]],X).
-% X = [[1, 2, 4, 6, 7, 6], [3, 4, 5, 9, 8, 8]] ;
+% ?- concatenate_lists_1D_axis0([[1,2],[3,4]],[[6,7],[9,8]],X).
+% X = [[1,2],[3,4],[6,7],[9,8]].
 
-concatenate_lists_2D([],[],[]).
-concatenate_lists_2D(X,[],X).
-concatenate_lists_2D([],Y, Y).
-concatenate_lists_2D([X|Xs],[Y|Ys],Z):-
- concatenate_lists_1D(X,Y,Zs),
+% ?- concatenate_lists_2_3_D_axis1([[1,2],[3,4]],[[6,7],[9,8]],X).
+% X = [[1,2,6,7],[3,4,9,8]] 
+
+%?- concatenate_lists_1D_axis0([[[0,1,2],[3,4,5]]],[[[0,1,2],[3,4,5]]],X).
+% X = [[[0,1,2],[3,4,5]],[[0,1,2],[3,4,5]]].
+
+% ?- concatenate_lists_2_3_D_axis1([[[0,1,2],[3,4,5]]],[[[0,1,2],[3,4,5]]],X).
+% X = [[[0,1,2],[3,4,5],[0,1,2],[3,4,5]]];
+
+concatenate_lists_2_3_D_axis1([], [], []).
+concatenate_lists_2_3_D_axis1(X, [], X).
+concatenate_lists_2_3_D_axis1([], Y, Y).
+concatenate_lists_2_3_D_axis1([X|Xs], [Y|Ys], Z):-
+ concatenate_lists_1D_axis0(X, Y, Zs),
  Z = [Zs|T],  
- concatenate_lists_2D(Xs, Ys, T).
+ concatenate_lists_2_3_D_axis1(Xs, Ys, T).
 
 % concatenate_lists_3D([[[1,0],[2,0]],[[1,2],[2,3]]],[[[1,2],[3,4]],[[1,2],[3,4]]],[],X).
 % X = [[[1, 0], [2, 0], [1, 2], [3, 4]], [[1, 2], [2, 3], [1, 2], [3, 4]]] ;
+
 % concatenate_lists_3D([[[0,1,3],[5,6,3]],[[10,11,3],[15,16,3]]],[[[20,21,3],[25,26,3]],[[30,31,3],[35,36,3]]],[],X).
 % X = [[[0, 1, 3], [5, 6, 3], [20, 21, 3], [25, 26, 3]], [[10, 11, 3], [15, 16, 3], [30, 31, 3], [35, 36, 3]]] ;
 
@@ -38,7 +49,10 @@ concatenate_lists_3D([], [], Z, Z).
 concatenate_lists_3D(X,  [], _, X).
 concatenate_lists_3D([], Y, _, Y).
 concatenate_lists_3D([[X|X0s]|Xs],[[Y|Y0s]|Ys], Z0s, Zs):-
- concatenate_lists_1D([X|X0s],[Y|Y0s],Z),
+ concatenate_lists_1D_axis0([X|X0s],[Y|Y0s],Z),
  append(Z0s, [Z], Z1s),
  writeln(Z1s),
  concatenate_lists_3D(Xs, Ys, Z1s, Zs).
+
+
+
