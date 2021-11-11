4afb2c6685d3d59f25c8008db184a4c222033c28
Wang Zilin <zilin_wang@mymail.sutd.edu.sg>
2021-11-11 23:42:21 +0800

    rescaling layer

--- src/helperlayer.pl
+++ src/helperlayer.pl
@@ -412,6 +412,16 @@ reshape_layer(Is,Ss,Os) :-
 						string_concat(S1,S2,S),
 						 throw(S)); 
 					(reshape_layer(Is1,Ss,OsT),pack_list(OsT,Os)))).*/
+
+
+
+rescaling_layer([I|Is],Scale,Offset,[O|Os]) :-
+	depth([I],1) ->
+		O is I * Scale + Offset,
+		rescaling_layer(Is,Scale,Offset,Os);
+		rescaling_layer(I,Scale,Offset,O),
+		rescaling_layer(Is,Scale,Offset,Os).
+rescaling_layer([],_,_,[]).
 			
 %recursive_split([3,3],[1,2,3,4,5,6,7,8,9],X).
 %recursive_split([],Is,Is).
